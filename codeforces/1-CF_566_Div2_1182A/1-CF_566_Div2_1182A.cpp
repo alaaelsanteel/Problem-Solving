2d20285c33af8c1d4ce33947fcc3d7e56b9c58dd
Alaa Elsanteel <78312680+alaaelsanteel@users.noreply.github.com>
2022-04-06 20:49:01 +0200

    My Solutions

---
+++ codeforces/1-CF_566_Div2_1182A/1-CF_566_Div2_1182A.cpp
@@ -0,0 +1,27 @@
+///*******************************THINK TWICE CODE ONCE*******************************///
+///*******************************NO CODE NO FOOD*******************************///
+#include <bits/stdc++.h>
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define loop(n) for(int i=0;i<n;i++)
+#define ll long long
+using namespace std;
+/*
+        Author: Alaa Elsanteel
+*/
+int main()
+{
+    //freopen("nearest.i","r",stdin);
+    fast;
+
+int n;
+cin>>n;
+if(n%2!=0)
+    cout<<0;
+else
+cout<<(ll)(pow(2,n/2));
+
+}
+
+    return 0;
+}
+
